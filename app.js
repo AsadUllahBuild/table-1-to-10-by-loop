40ac7b0a6316de053d4847b681617a12f602c0b4
AsadUllahBuild <asadbuild97@gmail.com>
2023-08-28 11:22:34 +0500

    Add table head

--- app.js
+++ app.js
@@ -21,21 +21,34 @@ for (let i = 11; i <= 20; i++) {
 }
 
 
-// const head3 = document.getElementById("head3");
-// for (let i = 1; i <= 10; i++) {
-//     head3.innerHTML += (<br />);
+const head3 = document.getElementById("head1");
+for (let i = 21; i <= 30; i++) {
+    // console.log(`${i} * ${1} = ${i * 1}`);
+    head3.innerHTML += (` <br />`);
 
-//     for (let j = 1; j <= 10; j++); {
-//         head3.innerHTML += (`${i} * ${j} = ${i * j} <br/>`);
-//     }
-// }
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`${i} * ${j} = ${i * j}`);
+        head3.innerHTML += (`${i} * ${j} = ${i * j} <br />`);
+
+    }
+}
 
 
-// const head4 = document.getElementById("head4");
-// for (let i = 1; i <= 10; i++); {
-//     head4.innerHTML += (<br />);
+const head4 = document.getElementById("head2");
+for (let i = 31; i <= 40; i++) {
+    head4.innerHTML += (`<br/>`);
 
-//     for (let j = 1; j <= 10; j++); {
-//         head4.innerHTML += (`${i} * ${j} = ${i * j} <br/>`);
-//     }
-// }
+    for (let j = 1; j <= 10; j++) {
+        head4.innerHTML += (`${i} * ${j} = ${i * j} <br/>`);
+    }
+}
+const head5 = document.getElementById("head2");
+for (let i = 41; i <= 100; i++) {
+    head5.innerHTML += (`<br/>`);
+
+    for (let j = 1; j <= 10; j++) {
+        head5.innerHTML += (`${i} * ${j} = ${i * j} <br/>`);
+    }
+}
+
+
